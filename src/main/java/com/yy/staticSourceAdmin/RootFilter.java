4423c02026bce130c169c703a97e6164f06b36cd
小交linux <yy3110044@foxmail.com>
2019-01-02 06:54:24 +0800

    fasdfasdgasdf

--- src/main/java/com/yy/staticSourceAdmin/RootFilter.java
+++ src/main/java/com/yy/staticSourceAdmin/RootFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebFilter("/*")
@@ -14,11 +15,13 @@ public class RootFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpReq = (HttpServletRequest)request;
 		HttpServletResponse httpResp = (HttpServletResponse)response;
 
-		String downloadFileName = request.getParameter("downloadFileName");
+		String downloadFileName = httpReq.getParameter("downloadFileName");
 		if(!Util.empty(downloadFileName)) {
-			httpResp.setHeader("Content-Disposition", "attachment;filename=" + Util.urlEncode(downloadFileName.trim()));
+			String uri = httpReq.getRequestURI();
+			httpResp.setHeader("Content-Disposition", "attachment;filename=" + Util.urlEncode(downloadFileName.trim() + Util.getSuffix(uri)));
 		}
 		
 		httpResp.setHeader("Access-Control-Allow-Origin", "*");//允许跨域访问
